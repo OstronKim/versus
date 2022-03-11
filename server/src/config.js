03a1609eb7078d0bdf14c893562890097cd538e8
Algot Sandahl <algot.sandahl@gmail.com>
2022-03-11 11:31:49 +0100

    More tries to fix heroku compilation

--- server/src/config.js
+++ server/src/config.js
@@ -4,14 +4,14 @@ module.exports = {
   filesDir: path.join(__dirname, '..', 'files'),
   ip: 'localhost',
   secretKey: process.env.SECRET_KEY || 'very-nice-dev-secret',
-  port: 3000,
+  port: process.env.PORT || 3000,
   development: {
     host: 'localhost',
     dialect: 'sqlite',
     storage: 'database.sqlite'
   },
   production: {
-    host: 'localhost',
+    host: process.env.HOST,
     dialect: 'sqlite',
     storage: 'database.sqlite'
   }
