bd6d7dfef3dce0a0671c248517632187c90cf88d
Algot Sandahl <algot.sandahl@gmail.com>
2022-03-11 14:41:45 +0100

    More tries to fix heroku compilation

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 export default {
   apiUrl: process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://exjobb-image-upscaling.herokuapp.com/',
+    : 'https://exjobb-image-upscaling.herokuapp.com',
   title: 'Versus',
   isUser: true
 }
