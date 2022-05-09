e5072dafa82d6a9337432d806bc8c65f7e4671dc
Algot Sandahl <algot.sandahl@gmail.com>
2022-05-09 09:26:14 +0200

    Add eval ended message

--- client/src/components/MturkParticipant.vue
+++ client/src/components/MturkParticipant.vue
@@ -1,7 +1,15 @@
 <template>
-  <div style="padding-left: 1em; padding-right: 1em;">
+  <div style="padding-left: 1em; padding-right: 1em;
+              text-align: center; padding-top: 3rem;">
+    <h2 class="md-display-2">Utvärderingen är avslutad</h2>
+    <p>Tack för visat intresse intresse :)</p>
 
-    <h2 class="md-display-2">
+    <br><br>
+
+    <h2 class="md-display-2">The evaluation has ended</h2>
+    <p>Thanks for your interest :)</p>
+
+    <!-- <h2 class="md-display-2">
       Creating new participant
     </h2>
 
@@ -11,12 +19,12 @@
 
     <p v-if="error">
       {{ error }}
-    </p>
+    </p> -->
   </div>
 </template>
 
 <script>
-import rpc from '../modules/rpc'
+// import rpc from '../modules/rpc'
 
 export default {
   name: 'experiment',
@@ -27,17 +35,17 @@ export default {
     }
   },
   async mounted () {
-    this.$data.experimentId = this.$route.params.experimentId
-    console.log('> MturkParticipant.mounted', this.$data.experimentId)
-    let response = await rpc.rpcRun(
-      'publicInviteParticipant', this.$data.experimentId, 'test@test.com')
-    if (response.result) {
-      console.log('> Experiment.makeInvite', response)
-      this.participateId = response.result.participant.participateId
-      this.$router.push('/participant/' + this.participateId)
-    } else {
-      this.error = response.error.message
-    }
+    // this.$data.experimentId = this.$route.params.experimentId
+    // console.log('> MturkParticipant.mounted', this.$data.experimentId)
+    // let response = await rpc.rpcRun(
+    //   'publicInviteParticipant', this.$data.experimentId, 'test@test.com')
+    // if (response.result) {
+    //   console.log('> Experiment.makeInvite', response)
+    //   this.participateId = response.result.participant.participateId
+    //   this.$router.push('/participant/' + this.participateId)
+    // } else {
+    //   this.error = response.error.message
+    // }
   }
 }
 </script>
